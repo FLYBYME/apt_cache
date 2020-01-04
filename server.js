6006e85da260877d567faaf674fadb8bfe25de0c
FLYBYME <nodetopia@gmail.com>
2020-01-04 03:21:27 +0000

    sad

--- server.js
+++ server.js
@@ -182,3 +182,14 @@ const proxy = http.createServer((req, res) => {
 });
 
 //proxy.listen(8080)
+const hostname = '127.0.0.1';
+const port = 8080;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
