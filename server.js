3a0702edcd7b25e1c0c391db47cead6132cb1bdd
FLYBYME <mangoraft@gmail.com>
2020-01-11 17:20:57 +0000

    sad

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const mime = require('mime');
 const hostnames = {
     'ubuntu.archive.nodetopia.xyz': '192.175.120.167',
     'debian.archive.nodetopia.xyz': '192.175.120.168',
-    'alpine.archive.nodetopia.xyz': '89.202.239.219'
+    'alpine.archive.nodetopia.xyz': '208.74.142.61'
 };
 
 let downloading = {};
